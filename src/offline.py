1eee0526415f1078d21f64c8d53c4505c9d23e92
Nick <129078463+nicholasjmorris1993@users.noreply.github.com>
2023-05-07 20:09:38 -0400

    Create offline.py

---
+++ src/offline.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+from sklearn.neighbors import LocalOutlierFactor
+
+
+def batching(df, test_frac):
+    model = Batching()
+    model.train(df, test_frac)
+    model.predict()
+
+    return model
+
+
+class Batching:
+    def train(self, df, test_frac):
+        self.data = df.copy()
+        self.test_frac = test_frac
+        train = self.data.copy().head(int(len(self.data)*(1 - self.test_frac)))
+
+        # train a model to detect outliers
+        self.model = LocalOutlierFactor(n_neighbors=20, leaf_size=30, novelty=True)
+        self.model.fit(train)
+
+    def predict(self):
+        test = self.data.copy().tail(int(len(self.data)*self.test_frac))
+
+        # find the outliers
+        score = self.model.predict(test)
+        is_anomaly = score == -1
+
+        self.predictions = pd.DataFrame({
+            "Score": score,
+            "Anomaly": is_anomaly,
+        })
